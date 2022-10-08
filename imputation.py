d1b1562a5c01dc5eebea9ec0ab97526082c02221
yaober <2576094621@qq.com>
2022-10-08 18:19:19 +0800

    update_for_specific_tools

--- imputation.py
+++ imputation.py
@@ -1,17 +1,37 @@
+import stlearn as st
 import numpy as np
-a = np.array([[1,3,2],
-             [0,2,5],
-             [2,2,0]],dtype=np.float32)
-b = np.where(a!=0,1,0)  #
-c = b.sum(axis=0) #count_zero
-a_mean = a.sum(axis=0)/c #average
-d = ~(b.astype(np.bool)) #select_zero
-d = a_mean * d.astype(np.float)
-result = a+d
+from scipy.sparse import csr_matrix
+adata_h5 = st.Read10X(path=r'./', count_file='151676'+'_filtered_feature_bc_matrix.h5' )
+#<class 'scipy.sparse._csr.csr_matrix'> to numpy
+data = adata_h5.X.toarray().astype(np.float64) #csr to dense
+#data = np.array([[0,0,1],[0,1,1],[0,2,1]])
+print("XXXXXXXXXXXXXXXXx")
+cnt_zero =  (np.count_nonzero(data))/(data.shape[0] * data.shape[1])
+print("data: \n", data,"\n", data.shape)
+print("cnt_zero: ", cnt_zero)
+data_bool = np.where(data != 0., 1., 0.)
+data_sum_percol = data_bool.sum(axis=0)
+print("data_sum_percol: ", data_sum_percol)
+data_sum_percol[data_sum_percol == 0] = 999.
+data_mean = data.sum(axis = 0) / data_sum_percol
+print("data_mean: ", data_mean)
+data_bool_reverse = ~(data_bool.astype(np.bool_))
+result = data_mean * data_bool_reverse.astype(np.float64)
+result = data + result
+k = csr_matrix(result, dtype=np.float64)
+adata_h5.X = k
 
-print("a:\n",a)
-print("b:\n",b)
-print("c:\n",c)
-print("a_mean:\n",a_mean)
-print("d:\n",d)
-print("result:\n",result)
+#check
+print("###############check###################")
+r_cnt_zero = result.shape[0] * result.shape[1] - np.count_nonzero(result)
+print("result: \n", result,"\n", result.shape)
+print("r_cnt_zero: ", r_cnt_zero)
+r_mean = result.mean(axis = 0)
+
+assert((r_mean - data_mean).sum() < 1e-10)
+s = k.toarray().astype(np.float64)
+
+assert((s!=result).sum()==0)
+
+
+
